fbec230103e9cbca6a02474ec5b9e3ec8fea77d1
dajaca <53102926+dajaca@users.noreply.github.com>
2019-07-27 08:27:56 -0400

    Initial commit

---
+++ src/main/java/com/dajaca/goodbye/servlet/GoodbyeServlet.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dajaca.goodbye.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author danie
+ */
+@WebServlet(name = "GoodbyeServlet", urlPatterns = {"/GoodbyeServlet"})
+public class GoodbyeServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet GoodbyeServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello , "+request.getParameter("to")+"</h1>");
+            out.println("<form action=\"/hello-servlet/hello\" method=\"get\">\n" +
+                        "  <div>\n" +
+                        "    <label for=\"to\">Who do you want to say it to?</label>\n" +
+                        "    <input name=\"to\" id=\"to\" value=\""+request.getParameter("to")+"\">\n" +
+                        "  </div>\n" +
+                        "  <div>\n" +
+                        "    <button>Say Goodbye!</button>\n" +
+                        "  </div>\n" +
+                        "</form>");
+            out.println("</body>");
+            out.println("</html>");
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
